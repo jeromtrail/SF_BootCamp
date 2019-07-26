138f95418575389f890e96bf7acb1b8dcd967382
jeromtrail <jerom.r.gonzaga@gmail.com>
2019-07-26 11:56:07 +0800

    Push to Production

---
+++ trailDX/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,96 @@
+declare module "@salesforce/resourceUrl/DTC_Account" {
+    var DTC_Account: string;
+    export default DTC_Account;
+}
+declare module "@salesforce/resourceUrl/DTC_Case" {
+    var DTC_Case: string;
+    export default DTC_Case;
+}
+declare module "@salesforce/resourceUrl/DTC_Event" {
+    var DTC_Event: string;
+    export default DTC_Event;
+}
+declare module "@salesforce/resourceUrl/DTC_Opportunity" {
+    var DTC_Opportunity: string;
+    export default DTC_Opportunity;
+}
+declare module "@salesforce/resourceUrl/DTC_OpportunityHistory" {
+    var DTC_OpportunityHistory: string;
+    export default DTC_OpportunityHistory;
+}
+declare module "@salesforce/resourceUrl/DTC_OpportunityLineItem" {
+    var DTC_OpportunityLineItem: string;
+    export default DTC_OpportunityLineItem;
+}
+declare module "@salesforce/resourceUrl/DTC_PriceBookEntry" {
+    var DTC_PriceBookEntry: string;
+    export default DTC_PriceBookEntry;
+}
+declare module "@salesforce/resourceUrl/DTC_Product2" {
+    var DTC_Product2: string;
+    export default DTC_Product2;
+}
+declare module "@salesforce/resourceUrl/DTC_Task" {
+    var DTC_Task: string;
+    export default DTC_Task;
+}
+declare module "@salesforce/resourceUrl/DTC_Telephony" {
+    var DTC_Telephony: string;
+    export default DTC_Telephony;
+}
+declare module "@salesforce/resourceUrl/DataManager_SalesWave_WorkflowEdits" {
+    var DataManager_SalesWave_WorkflowEdits: string;
+    export default DataManager_SalesWave_WorkflowEdits;
+}
+declare module "@salesforce/resourceUrl/HelloFunction" {
+    var HelloFunction: string;
+    export default HelloFunction;
+}
+declare module "@salesforce/resourceUrl/PleaseSayHello" {
+    var PleaseSayHello: string;
+    export default PleaseSayHello;
+}
+declare module "@salesforce/resourceUrl/Test_WorkflowDefinition" {
+    var Test_WorkflowDefinition: string;
+    export default Test_WorkflowDefinition;
+}
+declare module "@salesforce/resourceUrl/Test_WorkflowList" {
+    var Test_WorkflowList: string;
+    export default Test_WorkflowList;
+}
+declare module "@salesforce/resourceUrl/VFResource" {
+    var VFResource: string;
+    export default VFResource;
+}
+declare module "@salesforce/resourceUrl/countup7" {
+    var countup7: string;
+    export default countup7;
+}
+declare module "@salesforce/resourceUrl/heatmap" {
+    var heatmap: string;
+    export default heatmap;
+}
+declare module "@salesforce/resourceUrl/heatmapmock" {
+    var heatmapmock: string;
+    export default heatmapmock;
+}
+declare module "@salesforce/resourceUrl/leaflet" {
+    var leaflet: string;
+    export default leaflet;
+}
+declare module "@salesforce/resourceUrl/leaflet1" {
+    var leaflet1: string;
+    export default leaflet1;
+}
+declare module "@salesforce/resourceUrl/normalize" {
+    var normalize: string;
+    export default normalize;
+}
+declare module "@salesforce/resourceUrl/nouislider" {
+    var nouislider: string;
+    export default nouislider;
+}
+declare module "@salesforce/resourceUrl/style" {
+    var style: string;
+    export default style;
+}
